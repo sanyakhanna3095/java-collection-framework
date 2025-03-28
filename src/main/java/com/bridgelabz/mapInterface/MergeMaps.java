16c038e207e45771a45014e8ed04221341b39d92
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-28 22:17:01 +0530

    [Sanya] Add: Merge two maps such that if a key exists in both, sum their values

---
+++ src/main/java/com/bridgelabz/mapInterface/MergeMaps.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.mapInterface;
+
+import java.util.*;
+
+public class MergeMaps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        // Input for the first map
+        System.out.println("Enter number of key-value pairs for Map1:");
+        int n1 = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n1; i++) {
+            System.out.println("Enter key (String):");
+            String key = sc.nextLine();
+            System.out.println("Enter value (Integer):");
+            int value = sc.nextInt();
+            sc.nextLine();
+            map1.put(key, value);
+        }
+
+        // Input for the second map
+        System.out.println("Enter number of key-value pairs for Map2:");
+        int n2 = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n2; i++) {
+            System.out.println("Enter key (String):");
+            String key = sc.nextLine();
+            System.out.println("Enter value (Integer):");
+            int value = sc.nextInt();
+            sc.nextLine();
+            map2.put(key, value);
+        }
+
+        // Merge the maps, summing values if a key exists in both
+        Map<String, Integer> mergedMap = new HashMap<>(map1);
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            mergedMap.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+        System.out.println("Merged Map: " + mergedMap);
+
+    }
+}
+
