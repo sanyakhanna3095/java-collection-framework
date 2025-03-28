a4442f1de69a3c7f6eaa39bfd91ebb576915e101
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-28 19:40:56 +0530

    [Sanya] Add: Check if one set is a subset of another

---
+++ src/main/java/com/bridgelabz/setInterface/SubsetCheck.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.setInterface;
+
+import java.util.*;
+
+public class SubsetCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // take input for set1 and set2
+        System.out.println("Enter elements for Set1 (type 'done' to stop):");
+        Set<Integer> set1 = inputSet(sc);
+
+        System.out.println("Enter elements for Set2 (type 'done' to stop):");
+        Set<Integer> set2 = inputSet(sc);
+
+        // Check if set1 is a subset of set2
+        boolean isSubset = set2.containsAll(set1);
+        System.out.println("Is Set1 a subset of Set2? " + isSubset);
+    }
+
+    // Helper method to take input and return a set
+    private static Set<Integer> inputSet(Scanner sc) {
+        Set<Integer> set = new HashSet<>();
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equalsIgnoreCase("done")) break;
+            try {
+                set.add(Integer.parseInt(input));
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter integers.");
+            }
+        }
+        return set;
+    }
+}
+
+
+/*
+I/P->
+Enter elements for Set1 (type 'done' to stop):
+2 3
+done
+Enter elements for Set2 (type 'done' to stop):
+1 2 3 4
+done
+
+O/P->
+Is Set1 a subset of Set2? true
+ */
